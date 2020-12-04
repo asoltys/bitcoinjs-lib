250c2f0c580b2c141c589c92bfba2b3bbd35f2a2
Adam Soltys <asoltys@gmail.com>
2020-12-04 15:18:42 -0800

    esm module

---
+++ esm/wrapper.js
@@ -0,0 +1,14 @@
+import bitcoinjs from '../src/index.js';
+
+export const bip32 = bitcoinjs.bip32;
+export const address = bitcoinjs.address;
+export const crypto = bitcoinjs.crypto;
+export const ECPair = bitcoinjs.ECPair;
+export const networks = bitcoinjs.networks;
+export const payments = bitcoinjs.payments;
+export const script = bitcoinjs.script;
+export const Block = bitcoinjs.Block;
+export const Psbt = bitcoinjs.Psbt;
+export const opcodes = bitcoinjs.opcodes;
+export const Transaction = bitcoinjs.Transaction;
+export const TransactionBuilder = bitcoinjs.TransactionBuilder;
